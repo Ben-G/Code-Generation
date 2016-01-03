210f28faa8b79baf13923942034f67c9ae8cbbfa
Benji Encz <Benjamin.Encz@gmail.com>
2016-01-03 10:55:58 -0800

    Initial Commit

---
+++ SwiftFlowCodeGen/SwiftFlowCodeGen.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFlowCodeGen.h
+//  SwiftFlowCodeGen
+//
+//  Created by Benji Encz on 1/3/16.
+//  Copyright © 2016 Benji Encz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFlowCodeGen.
+FOUNDATION_EXPORT double SwiftFlowCodeGenVersionNumber;
+
+//! Project version string for SwiftFlowCodeGen.
+FOUNDATION_EXPORT const unsigned char SwiftFlowCodeGenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFlowCodeGen/PublicHeader.h>
+
+
